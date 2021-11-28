d7c8376ac52ffea3e2629bc9c58328f17c6f2ae0
bemer303 <52060533+bemer303@users.noreply.github.com>
2021-11-27 23:13:13 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import OneHotEncoder
+
+if __name__ == '__main__':
+
+    # load data into numpy array
+    data = np.loadtxt("adult.data", delimiter= ',', dtype=str)
+
+    # classification data is last column of data
+    y = np.array(data[:,14])
+
+    # remove last column from data
+    X = np.delete(data,14,1)
+
+    clf = GaussianNB()
+    # prints for reference
+    print(X[0])
+    print(X[:,0])
+
+    # convert age to int
+    for x in X[:,0]:
+        x = int(x)
+
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc.fit(X)
+    print(enc.categories_)
+   # clf.fit(X,y)
+
